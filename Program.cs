db91f504deaab43feba148c9f6624e587ed674aa
De1Mentos <zofinlel@gmail.com>
2023-06-23 20:37:04 +0300

    Switch Added

--- Program.cs
+++ Program.cs
@@ -8,24 +8,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Exercise ------- 1");
+            Console.WriteLine("Choose an exercise (1-8):");
+            int exerciseNumber = int.Parse(Console.ReadLine());
+
+            switch (exerciseNumber)
+            {
+                case 1:
+                    Exercise1();
+                    break;
+                case 2:
+                    Exercise2();
+                    break;
+                case 3:
+                    Exercise3();
+                    break;
+                case 4:
+                    Exercise4();
+                    break;
+                case 5:
+                    Exercise5();
+                    break;
+                case 6:
+                    Exercise6();
+                    break;
+                case 8:
+                    Exercise8();
+                    break;
+                default:
+                    Console.WriteLine("Invalid exercise number.");
+                    break;
+            }
+        }
+
+        static void Exercise1()
+        {
+            Console.WriteLine("Exercise 1");
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3, 2, 1 };
 
             int evenCount = numbers.Count(n => n % 2 == 0);
-
             int oddCount = numbers.Count(n => n % 2 != 0);
-
             int uniqueCount = numbers.Distinct().Count();
 
             Console.WriteLine("Even elements: " + evenCount);
             Console.WriteLine("Odd elements: " + oddCount);
             Console.WriteLine("Unique elements: " + uniqueCount);
+        }
 
-            Console.WriteLine("Exercise ------- 2");
+        static void Exercise2()
+        {
+            Console.WriteLine("Exercise 2");
             int[] numbers2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3, 2, 1 };
 
-            Console.WriteLine("Set the number");
+            Console.WriteLine("Enter a number:");
             int number = int.Parse(Console.ReadLine());
+
             for (int i = 0; i < numbers2.Length; i++)
             {
                 if (numbers2[i] > number)
@@ -33,10 +69,17 @@
                     Console.WriteLine(i);
                 }
             }
+        }
 
-            Console.WriteLine("Exercise ------- 3");
+        static void Exercise3()
+        {
+            Console.WriteLine("Exercise 3");
+            // Add your code for Exercise 3 here
+        }
 
-            Console.WriteLine("Exercise ------- 4");
+        static void Exercise4()
+        {
+            Console.WriteLine("Exercise 4");
             int[] array1 = { 1, 2, 3, 4, 5 };
             int[] array2 = { 4, 5, 6, 7, 8 };
 
@@ -46,12 +89,16 @@
             {
                 Console.WriteLine(element);
             }
-            Console.WriteLine("Exercise ------- 5");
+        }
+
+        static void Exercise5()
+        {
+            Console.WriteLine("Exercise 5");
             int[,] array = {
-            { 1, 2, 3 },
-            { 4, 5, 6 },
-            { 7, 8, 9 }
-        };
+        { 1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 }
+    };
 
             int rows = array.GetLength(0);
             int columns = array.GetLength(1);
@@ -73,22 +120,18 @@
 
             Console.WriteLine("Min: " + minValue);
             Console.WriteLine("Max: " + maxValue);
+        }
 
-            Console.WriteLine("Exercise ------- 6");
+        static void Exercise6()
+        {
+            Console.WriteLine("Exercise 6");
+            // Add your code for Exercise 6 here
+        }
 
-            Console.WriteLine("Type Sentance:");
-
-            string sentence = Console.ReadLine();
-
-            string[] words = sentence.Split(' ');
-
-            int wordCount = words.Length;
-
-            Console.WriteLine("Amount of words: " + wordCount);
-
-            Console.WriteLine("Exercise ------- 8");
-         
-            Console.WriteLine("Type Sentance:");
+        static void Exercise8()
+        {
+            Console.WriteLine("Exercise 8");
+            Console.WriteLine("Enter a sentence:");
             string sentence1 = Console.ReadLine();
 
             int vowelCount = CountVowels(sentence1);
