847c5242e0e7dee5c621174bc076d210e3b5d75d
De1Mentos <zofinlel@gmail.com>
2023-07-02 15:43:51 +0300

    йй

--- Program.cs
+++ Program.cs
@@ -1,10 +1,55 @@
-﻿namespace С__Pack
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace С__Pack
 {
     internal class Program
     {
+        public delegate void MessageDelegate(string message);
+
         static void Main(string[] args)
         {
-         
+            Console.WriteLine("Choose an exercise (1-4):");
+            int exerciseNumber = int.Parse(Console.ReadLine());
+            switch (exerciseNumber)
+            {
+                case 1:
+                    Exercise1();
+                    break;
+                case 2:
+                    Exercise2();
+                    break;
+                case 3:
+                    Exercise3();
+                    break;
+                case 4:
+                    Exercise4();
+                    break;
+            }
+        }
+
+        static void Exercise1()
+        {
+            MessageDelegate messageDelegate = DisplayMessage;
+
+            messageDelegate("FIRST MESSAGE");
+            messageDelegate("SECOND MESSAGE");
+
+            Console.ReadLine();
+        }
+
+        static void DisplayMessage(string message)
+        {
+            Console.WriteLine("Повідомлення: " + message);
+        }
+    static void Exercise2()
+        {
+        }
+        static void Exercise3()
+        {
+        }
+        static void Exercise4()
+        {
         }
     }
 }
