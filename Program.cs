12a3f80b27720940b3caf34c561718f43b98b2dc
De1Mentos <zofinlel@gmail.com>
2023-07-18 23:19:05 +0300

    qq

--- Program.cs
+++ Program.cs
@@ -2,6 +2,223 @@
 {
     using System.Collections.Generic;
 
+    public class LinkedList<T>
+    {
+        private class Node
+        {
+            public T Value { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+                Next = null;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+        private int count;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+
+        public void AddFirst(T value)
+        {
+            Node newNode = new Node(value);
+
+            if (IsEmpty)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+
+            count++;
+        }
+
+        public void AddLast(T value)
+        {
+            Node newNode = new Node(value);
+
+            if (IsEmpty)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+
+            count++;
+        }
+
+        public void RemoveFirst()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("List is empty");
+            }
+
+            if (head == tail)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                head = head.Next;
+            }
+
+            count--;
+        }
+
+        public void RemoveLast()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("List is empty");
+            }
+
+            if (head == tail)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != tail)
+                {
+                    current = current.Next;
+                }
+                current.Next = null;
+                tail = current;
+            }
+
+            count--;
+        }
+
+        public bool Contains(T value)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (current.Value.Equals(value))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+    }
+    public class CircularQueue<T>
+    {
+        private T[] queueArray;
+        private int front;
+        private int rear;
+        private int size;
+
+        public CircularQueue(int capacity)
+        {
+            queueArray = new T[capacity];
+            front = 0;
+            rear = -1;
+            size = 0;
+        }
+
+        public int Count
+        {
+            get { return size; }
+        }
+
+        public int Capacity
+        {
+            get { return queueArray.Length; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return size == 0; }
+        }
+
+        public bool IsFull
+        {
+            get { return size == queueArray.Length; }
+        }
+
+        public void Enqueue(T item)
+        {
+            if (IsFull)
+            {
+                throw new InvalidOperationException("Queue is full");
+            }
+
+            rear = (rear + 1) % queueArray.Length;
+            queueArray[rear] = item;
+            size++;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            T dequeuedItem = queueArray[front];
+            queueArray[front] = default(T);
+            front = (front + 1) % queueArray.Length;
+            size--;
+            return dequeuedItem;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return queueArray[front];
+        }
+
+        public void Clear()
+        {
+            Array.Clear(queueArray, 0, queueArray.Length);
+            front = 0;
+            rear = -1;
+            size = 0;
+        }
+    }
     public class PriorityQueue<T>
     {
         private List<T> elements;
@@ -150,9 +367,43 @@
             }
             static void Exercise3()
             {
+                CircularQueue<string> queue = new CircularQueue<string>(5);
+
+                queue.Enqueue("Елемент 1");
+                queue.Enqueue("Елемент 2");
+                queue.Enqueue("Елемент 3");
+
+                int count = queue.Count;
+
+                string firstItem = queue.Peek();
+
+                string dequeuedItem = queue.Dequeue();
+
+                count = queue.Count;
+
+                queue.Clear();
+
+                bool isEmpty = queue.IsEmpty;
             }
             static void Exercise4()
             {
+                LinkedList<int> list = new LinkedList<int>();
+
+                list.AddFirst(10);
+                list.AddLast(20);
+                list.AddLast(30);
+
+                int count = list.Count;
+
+                bool contains = list.Contains(20);
+
+                list.RemoveFirst();
+
+                list.RemoveLast();
+
+                list.Clear();
+
+                bool isEmpty = list.IsEmpty;
             }
             static void Exercise5()
             {
